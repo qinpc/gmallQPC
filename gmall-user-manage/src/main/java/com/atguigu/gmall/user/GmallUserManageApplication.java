f30efa4e6d9bcb1ffc4b9e210f4ed31a0bf115ed
qinpc <530468642@qq.com>
2019-09-05 00:08:45 +0800

    第一天

--- gmall-user-manage/src/main/java/com/atguigu/gmall/user/GmallUserManageApplication.java
+++ gmall-user-manage/src/main/java/com/atguigu/gmall/user/GmallUserManageApplication.java
@@ -1,9 +1,11 @@
 package com.atguigu.gmall.user;
 
+import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.gmall.user.mapper")
 public class GmallUserManageApplication {
 
 	public static void main(String[] args) {
